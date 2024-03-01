a246eccc69eecf5131049aadbe5ff836de305711
peihsinchang <peihsinchang@eztravel.com.tw>
2024-03-01 16:17:50 +0800

    feat: dates

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,4 +1,17 @@
+"use client" 
 import React from 'react';
+import styled from 'styled-components';
+
+
+const EmptyFunction= (datewww)=>{
+  console.log(datewww)
+  // let datesArr
+  // for(let i=0;i<dates;i++){
+  //   datesStr+=<div></div>
+  // }
+  // console.log({datesStr})
+  // return datesStr
+}
 
 
 export default function Calendar() {
@@ -6,19 +19,32 @@ export default function Calendar() {
   console.log(date.getDate())
   console.log(date.getMonth())
   console.log(date.getFullYear())
+  console.log('星期',date.getDay())
 
   const dates=new Date(date.getFullYear(),date.getMonth()+1, 0).getDate()
   const weekDateMap=['Sun','Mon','Tue','Wen','Thus','Fri','Sat']
 
+  const dateStr= EmptyFunction(date.getDay())
+  // console.log(dateStr)
+  for(let i=0;i<dates;i++){
+
+  }
   console.log({dates})
   // const dates=date.getDate(date.getFullYear(),date.getMonth())
-  dates
+  const datesArr=[]
 
   return (
-  <><h1>Calendar</h1>
+  <Wrapper><h1>Calendar</h1>
   <div>{date.toDateString()}</div>
+
+  {/* {dateStr} */}
   <div></div>
 
-  </>)
+  </Wrapper>)
   
 }
+
+
+const Wrapper=styled.div`
+  border: 2px solid red;
+`
